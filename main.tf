e970a15d0788d41c3b0299173b30a27ffee12379
Sara Handouf <jamal.marouane@gmail.com>
2025-10-28 06:05:46 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  cloud {
+    organization = "jamal-demo"
+    
+    workspaces {
+      name = "demo-gcp-network"
+    }
+  }
+  
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "tfe-demo-476501" 
+  region = "us-central1"
+}
+
+resource "google_compute_network" "demo" {
+  name                    = "tfe-demo-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "demo" {
+  name          = "tfe-demo-subnet"
+  ip_cidr_range = "10.100.0.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.demo.id
+}
+
+output "vpc_name" {
+  value = google_compute_network.demo.name
+}
+
+output "vpc_id" {
+  value = google_compute_network.demo.id
+}
